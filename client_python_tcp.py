7d4a4efd6680834f4b23511fe85e76533db31150
Kyle Stubbs <kylestubbs@ucsb.edu>
2020-10-21 18:36:54 -0700

    only need to remove some comments

--- client_python_tcp.py
+++ client_python_tcp.py
@@ -1,11 +1,23 @@
+#https://www.youtube.com/watch?v=3QiPPX-KeSc&ab_channel=TechWithTim
+#This is the video that I used to learn how to use TCP socket programming. I based the client and the server off of the video, but I made many changes.
+
 #https://www.afternerd.com/blog/python-string-contains/
+#I used this website to help me use the find() function, which helped me identify "<" in the middle of the input message.
+
 #https://www.freecodecamp.org/news/how-to-substring-a-string-in-python/
-#https://www.geeksforgeeks.org/reading-writing-text-files-python/
+#This website helped me create a substring of the string, which was helpful for sending the command to the left of the "<".
+
 #https://stackoverflow.com/questions/3432102/python-socket-connection-timeout
+#This helped me get a grasp on how to set timeouts when a socket is receiving data.
+
+#https://stackoverflow.com/questions/17667903/python-socket-receive-large-amount-of-data
+#This helped me create a while-loop to send a message greater than the buffer without having to send the length first.
+
+#https://www.geeksforgeeks.org/reading-writing-text-files-python/
+#I learned how to properly write the string into files from this site.
 
 import socket
 import sys
-import subprocess #might only need in server
 
 SERVER = input("Enter server name or IP address: ")
 
@@ -16,10 +28,9 @@ if PORT < 0 or PORT > 65535:
     print("Invalid port number.")
     sys.exit()
 
-HEADER = 64
 FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
 ADDR = (SERVER, PORT)
+BUFFER_SIZE = 2048
 
 try:
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -27,46 +38,37 @@ try:
 except:
     print("Could not connect to server")
     sys.exit()
-
-def send(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-    client.send(send_length)
-    client.send(message)
-    #print(client.recv(2048).decode(FORMAT))
-    #client.recv(2048).decode(FORMAT)
     
-    #add ACK validation here
-
-
 message = input("Enter command: ")
 
 index = message.find(">")
 textFile = 'writtenFileTCP.txt'
-if index > 0:
-    send(message[0:index-1])
+if index > 0:       #if ">" exists
+    client.send(message[0:index-1].encode(FORMAT))
     textFile = message[index+2:len(message)]
 
 else:
-    send(message)
+    client.send(message.encode(FORMAT))
 
 try:
-    client.settimeout(.5)
-    result = client.recv(2048).decode(FORMAT)
+    currentSize = 0
+    sentMessage = b''
+    while True:     #loop continues until all the data has been received
+        client.settimeout(.5)
+        messagePacket = client.recv(BUFFER_SIZE)
+        sentMessage += messagePacket
+        if len(messagePacket) < BUFFER_SIZE:
+            break
+    assert(sentMessage != b'')  #check if no message was sent
 except:
     print("\nDid not receive response.")
     sys.exit()
     
     
-
+finalResult = sentMessage.decode(FORMAT)
 file1 = open(textFile,"w")
-file1.writelines(result)
+file1.writelines(finalResult)
 file1.close()
 print("\nFile " + textFile + " saved.")
 
-#send(message)
-
-#send(DISCONNECT_MESSAGE)
 client.close()
--- client_python_udp.py
+++ client_python_udp.py
@@ -1,87 +1,86 @@
+#https://www.freecodecamp.org/news/how-to-substring-a-string-in-python/
+#This website helped me create a substring of the string, which was helpful for sending the command to the left of the "<".
+
+#https://www.geeksforgeeks.org/reading-writing-text-files-python/
+#I learned how to properly write the string into files from this site.
+
 import socket
 import sys
-import subprocess
-import time
 
-#SERVER = input("Enter server name or IP address: ")
-SERVER = 'localhost'
+SERVER = input("Enter server name or IP address: ")
 
-#tempPort = input("Enter port: ")
-#PORT = int(tempPort)
-PORT = 5555
+tempPort = input("Enter port: ")
+PORT = int(tempPort)
 
 if PORT < 0 or PORT > 65535:
     print("Invalid port number.")
     sys.exit()
 
-HEADER = 64
 ACK = 'ACK'
 FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
 ADDR = (SERVER, PORT)
+BUFFER_SIZE = 2048
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-#message = input("Enter command: ")
-message = 'dir'
+message = input("Enter command: ")
 
 index = message.find(">")
 textFile = 'writtenFileUDP.txt'
 
-def checkACK():
+def checkACK(): #checks if ACK was sent from server
     try:
         client.settimeout(1)
-        ACKmessage, serverADDR = client.recvfrom(2048)
+        ACKmessage, serverADDR = client.recvfrom(BUFFER_SIZE)
         return True
-    except ConnectionResetError:
+    except ConnectionResetError: #initially checks if server and port are able to connect
         print("Could not connect to server.")
-        client.close()
         sys.exit()
     except socket.timeout:
         return False
 
 for i in range(4):
-    if index > 0:
+    if index > 0:   #if "<" exists
         length = str(len(message[0:index-1]))
         client.sendto(length.encode(FORMAT), ADDR)
         client.sendto(message[0:index-1].encode(FORMAT), ADDR)
         textFile = message[index+2:len(message)]
-
     else:
         length = str(len(message))
         client.sendto(length.encode(FORMAT), ADDR)
         client.sendto(message.encode(FORMAT), ADDR)
     
     returnedACK = checkACK()
+
     if returnedACK:
         break
     else:
-        if (i == 3):
+        if (i == 3):    #fails after 3 retries
             print("\nFailed to send command. Terminating.")
-            client.close()
             sys.exit()
         continue
 
-msgLength, serverADDR = client.recvfrom(2048)
+msgLength, serverADDR = client.recvfrom(BUFFER_SIZE)
 msgLength = int(msgLength.decode(FORMAT))
 msgString = b''
 
 previousMessagePacket = b''
 didNotReceiveMessagePacketCounter = 0
-while msgLength > len(msgString.decode(FORMAT)):   
+while msgLength > len(msgString.decode(FORMAT)):    #loop continues until the client receives the full message
     try:
-        client.settimeout(.5) #No clear instructions for this part so I am using .5 seconds like before
-        msgPacket, clientADDR = client.recvfrom(2048)
+        client.settimeout(.5)   #No clear instructions for this part so I am using .5 seconds like before
+        msgPacket, clientADDR = client.recvfrom(BUFFER_SIZE)
         client.sendto(ACK.encode(FORMAT), ADDR)
-        if (msgPacket != previousMessagePacket):
+
+        if (msgPacket != previousMessagePacket): #keeps track of the last sent packet to make sure the server didn't resend the same packet
             previousMessagePacket = msgPacket
-        else: #if the length or a packet is resent
+        else:                   #if the length or a packet is resent
             continue
         msgString += msgPacket
+
     except:
-        if didNotReceiveMessagePacketCounter == 3:
+        if didNotReceiveMessagePacketCounter == 3: #after 3 tries it gives up
             print("\nDid not receive response.")
-            client.close()
             sys.exit()
         else:
             didNotReceiveMessagePacketCounter += 1
@@ -92,5 +91,3 @@ file1 = open(textFile,"w")
 file1.writelines(msgString.decode(FORMAT))
 file1.close()
 print("\nFile " + textFile + " saved.")
-
-client.close()
